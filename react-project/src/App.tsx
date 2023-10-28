f5f00e476fc8aac6b8c9a2a04058b4c61cea26d3
baranovdv <baranov.d.v.1988@gmail.com>
2023-10-29 00:29:57 +0700

    feat: update App component

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,5 +1,15 @@
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+
 function App() {
-  return <></>;
+  return (
+    <section className="grid grid-rows-[auto_1fr_auto] bg-slate-100 h-screen">
+      <Header />
+      <Main />
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
