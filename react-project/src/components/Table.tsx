d86ab40c272ed2c93e4322451852be2eba622825
baranovdv <baranov.d.v.1988@gmail.com>
2023-10-29 00:29:02 +0700

    feat: add Table component

---
+++ react-project/src/components/Table.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import { TableRow } from '../data/types';
+
+type TableProps = {
+  headers: string[];
+  data?: TableRow[];
+};
+
+export class Table extends Component<TableProps> {
+  render() {
+    return (
+      <table className="w-full p-1 border-4 border-red-800">
+        <thead>
+          <tr className="p-1 border-2 border-red-800">
+            {this.props.headers.map((item) => (
+              <th key={item} className="p-1 border-2 border-red-800">
+                {item}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.data?.map((item) => (
+            <tr key={item.id} className="p-1 border-2 border-red-400">
+              <td className="p-1 border-2 border-red-400">{item.name}</td>
+              <td className="p-1 border-2 border-red-400 text-center font-semibold">
+                {item.description}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
