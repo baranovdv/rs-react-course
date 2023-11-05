c153a75952338a0f670c2a9413877e3d4bbd338e
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-06 03:05:47 +0700

    refactor: refactor app

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,14 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
-import { Main } from './components/Main';
 
 function App() {
   return (
     <section className="grid grid-rows-[auto_1fr_auto] bg-slate-100 h-screen">
       <ErrorBoundary>
         <Header />
-        <Main />
+        <Outlet />
         <Footer />
       </ErrorBoundary>
     </section>
