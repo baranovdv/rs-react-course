9b579fa7b3cfc7d1f0acf953ee228bef8c469a9e
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-06 03:06:01 +0700

    feat: add routing

--- react-project/src/main.tsx
+++ react-project/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import Main from './components/Main';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <></>,
+    children: [
+      {
+        path: '/',
+        element: <Main />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
