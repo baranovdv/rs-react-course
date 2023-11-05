6ecd7224d2ce9b5890a3b30eb0ae6a40f478d701
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-06 03:01:35 +0700

    refactor: refactor to functional component

--- react-project/src/components/ui/Input.tsx
+++ react-project/src/components/ui/Input.tsx
@@ -1,25 +1,23 @@
-import { ChangeEvent, Component } from 'react';
+import { ChangeEvent } from 'react';
 
-type InputProps = {
+interface InputProps {
   value: string;
   setValue: (s: ChangeEvent<HTMLInputElement>) => void;
-  label?: string;
-};
+  label: string;
+}
 
-export class Input extends Component<InputProps> {
-  render() {
-    return (
-      <>
-        <label className="flex items-center text-2xl" htmlFor="input">
-          {this.props.label || ''}
-        </label>
-        <input
-          id="input"
-          value={this.props.value}
-          onChange={(e) => this.props.setValue(e)}
-          className="w-[15%] px-1 border-2 border-red-800"
-        ></input>
-      </>
-    );
-  }
+export function Input({ value, setValue, label }: InputProps) {
+  return (
+    <>
+      <label className="flex items-center text-2xl" htmlFor="input">
+        {label || ''}
+      </label>
+      <input
+        id="input"
+        value={value}
+        onChange={(e) => setValue(e)}
+        className="w-[15%] px-1 border-2 border-red-800"
+      ></input>
+    </>
+  );
 }
