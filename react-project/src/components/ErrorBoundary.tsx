c27bef0bb1c882889ff2872794b3cd66e884b98e
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-06 03:03:13 +0700

    refactor: refactor constants

--- react-project/src/components/ErrorBoundary.tsx
+++ react-project/src/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import { Component, ErrorInfo, ReactNode } from 'react';
+import { ERROR_DATA } from '../data/data';
 import { Button } from './ui/Button';
 
 type State = {
@@ -15,16 +16,14 @@ export class ErrorBoundary extends Component<{
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Error caught by ErrorBoundary:', error, errorInfo);
+    console.error(ERROR_DATA.consoleMessage, error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
       return (
         <div className="py-8 flex flex-col gap-6 items-center bg-slate-300 h-screen">
-          <h1 className="text-2xl font-bold">
-            Something went wrong. Please try again later.
-          </h1>
+          <h1 className="text-2xl font-bold">{ERROR_DATA.htmlMessage}</h1>
           <Button onClick={() => window.location.reload()}>Refresh</Button>
         </div>
       );
