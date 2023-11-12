0315d8a9b10708f5a038c191611f1a78c909fa56
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-13 00:27:54 +0700

    feat: add fetch mock handler

--- react-project/src/mocks/handlers.ts
+++ react-project/src/mocks/handlers.ts
@@ -1,8 +1,26 @@
 import { http, HttpResponse } from 'msw';
-import { mockData } from './mockData';
+import { mockData, mockNotFound } from './mockData';
 
-export const handlers = [
-  http.get('https://rickandmortyapi.com/api/character/', () => {
+let isAPICalled: boolean = false;
+
+const clearIsAPICalled = () => (isAPICalled = false);
+
+const handlers = [
+  http.get('https://rickandmortyapi.com/api/character/', ({ request }) => {
+    isAPICalled = true;
+    const url = new URL(request.url);
+    const query = url.searchParams.get('name');
+
+    if (query === 'errorQuery') return HttpResponse.json(mockNotFound);
+
     return HttpResponse.json(mockData);
   }),
+  http.get('https://rickandmortyapi.com/api/character/:id', ({ params }) => {
+    isAPICalled = true;
+    const { id } = params;
+
+    return HttpResponse.json(mockData.results[Number(id) - 1]);
+  }),
 ];
+
+export { handlers, isAPICalled, clearIsAPICalled };
