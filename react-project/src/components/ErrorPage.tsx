e108341f3383caa9d6e7cccd20dd71d46148454b
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-13 02:00:04 +0700

    refactor: refactor error page

--- react-project/src/components/ErrorPage.tsx
+++ react-project/src/components/ErrorPage.tsx
@@ -1,11 +1,38 @@
 import { FC } from 'react';
-import { ERROR_DATA } from '../data/data';
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+
+import pic404 from '../assets/img/pic404.svg';
+import { Button } from './ui/Button';
+
+enum ErrorCodes {
+  'NOT_FOUND' = 404,
+}
 
 const ErrorPage: FC = () => {
+  const error = useRouteError();
+  let errorMessage = 'Unknown error';
+  let errorStatus = 0;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText;
+    errorStatus = error.status;
+  }
   return (
-    <div className="text-center mt-10 text-4xl">
-      {ERROR_DATA.errorPageMessage}
-    </div>
+    <>
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="text-2xl text-center">Error page</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{errorMessage}</i>
+        </p>
+        {errorStatus === ErrorCodes.NOT_FOUND && (
+          <img className="w-[120px]" src={pic404} alt="404" />
+        )}
+        <Link className="py-10" to="/">
+          <Button onClick={() => {}}>HOME</Button>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- react-project/src/components/Header.tsx
+++ react-project/src/components/Header.tsx
@@ -4,7 +4,6 @@ import Logo from '../assets/img/Rick_and_Morty.png';
 const Header: FC = () => {
   return (
     <header className="flex justify-center h-[80px] py-4 bg-slate-500">
-      <h1>Hello World</h1>
       <img src={Logo} alt="Logo" />
     </header>
   );
