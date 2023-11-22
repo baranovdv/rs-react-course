8f46b5fec360c110247073d84d1b92c70b130f9e
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-22 14:44:19 +0700

    fix: fix vitest bug

--- react-project/vite.config.ts
+++ react-project/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    useAtomics: true,
   },
   define: {
     global: 'window',
