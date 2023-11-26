92820df58b8c0a5b8529ff8e234d5449108d98b3
baranovdv <baranov.d.v.1988@gmail.com>
2023-11-26 11:22:08 +0700

    feat: update Input

---
+++ nextjs-project/src/components/ui/Select.tsx
@@ -0,0 +1,33 @@
+import { type SelectData } from '@/data/types';
+import { type FC } from 'react';
+
+interface SelectProps {
+  data: SelectData;
+  selected: number;
+  onSelect: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const Select: FC<SelectProps> = ({ data, selected, onSelect }) => {
+  return (
+    <label>
+      {`${data.selectLabel} `}
+      <select
+        className="p-1 border-2 border-red-800 bg-red-100"
+        defaultValue={selected}
+        onChange={(event: React.ChangeEvent<HTMLSelectElement>): void => {
+          onSelect(event);
+        }}
+      >
+        {data.selectOptions.map((item: number) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </label>
+  );
+};
+
+export { Select };
