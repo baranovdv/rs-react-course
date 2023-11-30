a96f350c2753e3e4b0533db660f728f231907c7b
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-01 00:44:53 +0700

    feat: add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { Main } from './pages/Main';
+import { Reacthook } from './pages/Reacthook';
+import { Uncontrolled } from './pages/Uncontrolled';
+import { store } from './store/store';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      { path: '/', element: <Main /> },
+      {
+        path: '/uncontrolled',
+        element: <Uncontrolled />,
+      },
+      { path: '/reacthook', element: <Reacthook /> },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
