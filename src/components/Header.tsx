b524805bf5d675e88acacedf205f5f860d4b3d95
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-01 00:43:04 +0700

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <header className="flex justify-center h-[80px] py-4 bg-slate-500"></header>
+  );
+};
+
+export { Header };
