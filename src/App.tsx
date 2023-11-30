43e9efa02d8588f2ecf06a3229e4da0219f948fb
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-01 00:45:50 +0700

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
 function App() {
-  return <h1 className=" bg-red-900">Hello</h1>;
+  return (
+    <section className="grid grid-rows-[auto_1fr_auto] bg-slate-300 h-screen">
+      <Header />
+      <Outlet />
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
