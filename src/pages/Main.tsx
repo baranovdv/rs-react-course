0c264fad61d9de87acf25539bcc70c2ceb82f8ab
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-03 20:31:07 +0700

    feat: add Main component

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,32 +1,67 @@
+import { nanoid } from '@reduxjs/toolkit';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Result } from '../components/Result';
-import { MyFormData } from '../data/types';
-
-const mockData: MyFormData = {
-  name: 'Testname',
-  age: 66,
-  email: 'test@test.ww',
-  gender: 'Male',
-};
+import { Button } from '../components/ui/Button';
+import { ROUTES } from '../data/data';
+import { FormType } from '../data/types';
+import { useAppSelector } from '../store/hooks';
+import { selectResultsStore } from '../store/resultsSlice';
 
 const Main: FC = () => {
   const navigate = useNavigate();
 
-  const UpdateUncontrolledHandler = () => {
-    navigate('/uncontrolled');
+  const resultsArray = useAppSelector(selectResultsStore);
+
+  const createFormHandler = (type: FormType): void => {
+    const id = nanoid();
+    if (type === 'uncontrolled') {
+      navigate(`${ROUTES.uncontrolled}/${id}`);
+    }
+    if (type === 'reacthook') {
+      navigate(`${ROUTES.reacthook}/${id}`);
+    }
   };
 
-  const UpdateReacthookHandler = () => {
-    navigate('/reacthook');
+  const updateFormHandler = (type: FormType, id: string): void => {
+    if (type === 'uncontrolled') {
+      navigate(`${ROUTES.uncontrolled}/${id}`);
+    }
+    if (type === 'reacthook') {
+      navigate(`${ROUTES.reacthook}/${id}`);
+    }
   };
 
   return (
     <main>
       <h1 className="text-center font-bold text-3xl my-3">Main page</h1>
-      <section className="grid grid-cols-1 gap-4 mx-4 sm:grid-cols-2">
-        <Result data={mockData} onUpdateClick={UpdateUncontrolledHandler} />
-        <Result data={mockData} onUpdateClick={UpdateReacthookHandler} />
+      <nav className="flex justify-around mt-4 mb-10">
+        <Button
+          className="w-[30%]"
+          onClick={() => createFormHandler('uncontrolled')}
+        >
+          Create Uncontrolled Form
+        </Button>
+        <Button
+          className="w-[30%]"
+          onClick={() => createFormHandler('reacthook')}
+        >
+          Create Reacthook Form
+        </Button>
+      </nav>
+      <section className="grid grid-cols-1 gap-4 m-4 sm:grid-cols-2">
+        {resultsArray.map((result) => {
+          return (
+            <Result
+              key={result.id}
+              title={`${result.formType} form`}
+              data={result}
+              onUpdateClick={() =>
+                updateFormHandler(result.formType, result.id)
+              }
+            />
+          );
+        })}
       </section>
     </main>
   );
