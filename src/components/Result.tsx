c5b3ae992fb850f955af1756d2029b98ac8f4390
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-03 20:31:20 +0700

    feat: add Result component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,24 +1,64 @@
-import { FC } from 'react';
-import { MyFormData } from '../data/types';
+import { FC, useEffect, useState } from 'react';
+import { MyResultData } from '../data/interfaces';
 import { Button } from './ui/Button';
 
 interface ResultProps {
   onUpdateClick: (event: React.MouseEvent<HTMLElement>) => void;
-  data: MyFormData;
+  title: string;
+  data: MyResultData;
 }
 
 const BUTTON_TITLE = 'Update';
+const NEW_RESULT_BGCOLOR = 'bg-green-300';
 
-const Result: FC<ResultProps> = ({ onUpdateClick, data }) => {
-  const { name, age, email, gender } = data;
+const Result: FC<ResultProps> = ({ onUpdateClick, title, data }) => {
+  const {
+    name,
+    password,
+    age,
+    email,
+    gender,
+    tandc,
+    country,
+    imageBase64,
+    isLast,
+  } = data;
+  const [bgColor, setBgColor] = useState('');
+
+  useEffect(() => {
+    let timeoutID: number | null = null;
+    if (isLast) {
+      setBgColor(NEW_RESULT_BGCOLOR);
+      timeoutID = setTimeout(() => {
+        setBgColor('');
+      }, 1000);
+    }
+    return () => {
+      if (timeoutID) clearTimeout(timeoutID);
+    };
+  }, [isLast]);
 
   return (
-    <div className="flex flex-col gap-1 p-6 items-center w-full rounded-[5%] bg-[rgba(255,255,255,0.3)] text-[rgba(71,71,71)]">
-      {/* <img className="w-[40%] rounded-[50%] m-2" src={image} alt="image" /> */}
-      <h2 className="text-2xl font-bold py">{name}</h2>
+    <div
+      className={`flex flex-col gap-1 p-6 items-center w-full rounded-[5%] bg-[rgba(255,255,255,0.3)] text-[rgba(71,71,71)] transition-all; ${bgColor}`}
+    >
+      <h2 className="text-2xl font-bold py-1">
+        {title.charAt(0).toUpperCase() + title.slice(1)}
+      </h2>
+      <img
+        className="w-[40%] rounded-[50%] m-2"
+        src={imageBase64}
+        alt="image"
+      />
+      <h3 className="font-semibold self-start">Name: {name}</h3>
+      <h3 className="font-semibold self-start">Password: {password}</h3>
       <h3 className="font-semibold self-start">Age: {age}</h3>
       <h3 className="font-semibold self-start">E-mail: {email}</h3>
       <h3 className="font-semibold self-start">Gender: {gender}</h3>
+      <h3 className="font-semibold self-start">
+        T & C: {tandc ? 'OK' : 'FAIL'}
+      </h3>
+      <h3 className="font-semibold self-start">Country: {country}</h3>
       <Button onClick={onUpdateClick}>{BUTTON_TITLE}</Button>
     </div>
   );
